5c76217a83c466681377601e9f8169cf1e068d98
ChrisRodrigoSB <christianrodrigo365gmail.com>
2025-06-03 21:39:55 -0300

    AULA01JAVASCRIPT EM ANDAMENDO

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,30 @@
+var idade
+//variaveis mlkote
+console.log("Hello World") //apresentar uma mensagem
+
+console.clear //limpar
+var numero01 = 192
+console.log(numero01 )
+
+var num1 = 4
+var num2 = 800000000000000000000000000000000000000000000000000000000000000000000000000000000008
+var resultado= num1+num2 
+console.log(resultado) //
+
+idade=89
+console.log(idade) //iniciar a variavel em outra parte do codigo 
+
+//textos 
+var nome01= 'matheus araujo'
+var nome02= "matheus vinicius"
+var nome03= "12365.7498"//otext
+console.log(nome01,nome02,nome03)
+
+VAR // VAR É FLEXIVEL E COM MENOS REGRAS, OU SEJA POSSO ALTERAR O VALOR INICIAL DE UMA VAR E RECRIAR A MESMA EM PARTES DE CODIGO
+
+LET// JÁ A LET VOCE NÃO PODE RECRIAR A MESMA EM OUTRAS PARTES DO COGIGO COM O MESMO NOME, MAS PODE CHAMAR E ALTERAR O VALOR INICIALLET 
+
+CONST// VOCE NÃO PODE RERIAR A MESMA EM OUTRAS PARTES DO CODIGO E NÃO PODE ALTERAR O VALOR INICIAL DELA GARANTINDO A CONFIANÇA DO DADO QUE ESTA SENDO ULTILIZADO AO DECORRER DO SEU CODIGO 
+
+
+
