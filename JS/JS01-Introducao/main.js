61b46857634476d433819263d1fbb36b5a1a0778
ChrisRodrigoSB <christianrodrigo365gmail.com>
2025-06-04 21:28:05 -0300

    AULA01JAVASCRIPT EM ANDAMENTO

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -20,11 +20,46 @@ var nome02= "matheus vinicius"
 var nome03= "12365.7498"//otext
 console.log(nome01,nome02,nome03)
 
-VAR // VAR É FLEXIVEL E COM MENOS REGRAS, OU SEJA POSSO ALTERAR O VALOR INICIAL DE UMA VAR E RECRIAR A MESMA EM PARTES DE CODIGO
+console.log("olá" + "papai") //concatenar
+console.log("Aluno" + nome01 + "e Aluno" + nome02 + "tem um total de" + nome03 + "dinheiros")
 
-LET// JÁ A LET VOCE NÃO PODE RECRIAR A MESMA EM OUTRAS PARTES DO COGIGO COM O MESMO NOME, MAS PODE CHAMAR E ALTERAR O VALOR INICIALLET 
+//TEMPLATE STRING
+console.log(`Aluno ${nome01} e Aluno ${nome02} tem um total de ${nome03} dinheiros`) //concatenar
+//VAR // VAR É FLEXIVEL E COM MENOS REGRAS, OU SEJA POSSO ALTERAR O VALOR INICIAL DE UMA VAR E RECRIAR A MESMA EM PARTES DE CODIGO
 
-CONST// VOCE NÃO PODE RERIAR A MESMA EM OUTRAS PARTES DO CODIGO E NÃO PODE ALTERAR O VALOR INICIAL DELA GARANTINDO A CONFIANÇA DO DADO QUE ESTA SENDO ULTILIZADO AO DECORRER DO SEU CODIGO 
+//LET// JÁ A LET VOCE NÃO PODE RECRIAR A MESMA EM OUTRAS PARTES DO COGIGO COM O MESMO NOME, MAS PODE CHAMAR E ALTERAR O VALOR INICIALLET 
 
+//CONST// VOCE NÃO PODE RERIAR A MESMA EM OUTRAS PARTES DO CODIGO E NÃO PODE ALTERAR O VALOR INICIAL DELA GARANTINDO A CONFIANÇA DO DADO QUE ESTA SENDO ULTILIZADO AO DECORRER DO SEU CODIGO 
 
+console.log(10/0)
+console.log(-6/0)
 
+//operadores
+
+var valor1 = 5
+var valor2 = 5
+
+console.log(valor1 + valor2)
+console.log(valor1 - valor2)
+console.log(valor1 * valor2)
+console.log(valor1 / valor2)
+console.log(valor1 ** valor2)
+
+//operadores relacionais 
+
+console.log(5 ==5 )
+console.log(5 > 5)
+console.log(22<8)
+console.log(22 >= 22)
+console.log(22 != 23);
+console.log("ana" == 'Ana');
+console.log(true === true);
+//outro e txt> String
+console.log(true != 'false');
+
+let temCamisa = false
+let temCracha = true
+console.log(temCamisa);
+console.log(temCracha);
+console.log(temCamisa == true && temCracha == true)
+console.log(temCamisa == true || temCracha == true);//false      ==      true
