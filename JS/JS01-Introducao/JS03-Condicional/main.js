bf1653d6f151000c7929d26da1e5a31f41d14951
ChrisRodrigoSB <christianrodrigo365gmail.com>
2025-06-12 18:27:49 -0300

    ExercicioCasa02JS

---
+++ JS/JS01-Introducao/JS03-Condicional/main.js
@@ -0,0 +1,18 @@
+let atividadeEntregue = "true "
+let diaFinalEntregue = 13
+if(atividadeEntregue== true){
+console.log("Parabéns, fera");
+}
+if(diaFinalEntregue == 13){
+console.log('entregou a atividade na data a sua nota é 10');
+}
+else (diaFinalEntregue){
+    console.log('não entregou -2 pontos');
+} 
+else if(atividadeEntregue == false){
+    console.log("vacilou, amigão");
+}
+
+else{
+    console.log("estamos com um pronlema");
+}
---
+++ JS/JS02-String/ExercicioCasaJS02/main.js
@@ -0,0 +1,13 @@
+let frase = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+// Caracter na posição 19 (índice começa do 0)
+console.log(frase[19]);
+
+// Tamanho da string
+console.log(frase.length);
+
+// Toda a string em maiúsculo
+console.log(frase.toUpperCase());
+
+// Apenas a palavra "Devs"
+console.log(frase.slice(-4));
