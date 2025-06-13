afa06226645a0c54362b8c088e29cbf5dc28043b
ChrisRodrigoSB <christianrodrigo365gmail.com>
2025-06-12 21:06:43 -0300

    ExercicioCasaRepetição

--- JS/JS01-Introducao/JS03-Condicional/ExercicioSala/main.js
+++ JS/JS01-Introducao/JS03-Condicional/ExercicioSala/main.js
@@ -6,4 +6,4 @@ if (numero1 > numero2) {
   console.log("numero1 é maior que numero2");
 } else if (numero1 < numero2) {
   console.log("numero1 é menor que numero2");
-} 
+} ,
---
+++ JS/JS04-Repetição/main.js
@@ -0,0 +1,11 @@
+let contador = 0 
+// enquanto a varial contador for menor que 10 o while apresentara o valor da variavel console 
+while(contador < 10){
+console.log(contador);
+contador++
+} console.log("finalizou o programa");
+
+contador = 0
+for(contador = 0; contador <= 10; contador++ ){
+    console.log(contador);
+}
